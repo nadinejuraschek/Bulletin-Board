c2aefaa19430f289ea24d9854ba74d2c3ca2c68c
Nadine Ju <nadine.jura@online.de>
2020-03-21 16:17:40 -0700

    Header, footer and first example note created

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+// REACT
+import React from 'react';
+
+// COMPONENTS
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+// REACT
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// COMPONENTS
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
